7128aeae15fdbd7e5dbdc5b0e3e95a43443eb6d1
Govind maithil <govindmaithilrtn@gmail.com>
2022-07-24 10:40:42 +0530

    TodoLists

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Users from './Users';
+import User1 from './User1';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello World</h1>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/User1.js
@@ -0,0 +1,9 @@
+function User1 ()
+{
+   return(
+      <div>
+         <h1>hello user 1</h1>
+      </div>
+   )
+}
+export default User1
